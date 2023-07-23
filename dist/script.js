049dfb11776ed0c9e5d28629400f5343ec4d2ada
KoFE <petrovykh03@gmail.com>
2023-07-23 15:46:15 +0500

    Requests module added

--- dist/script.js
+++ dist/script.js
@@ -440,6 +440,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_tab__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/tab */ "./src/js/lib/components/tab.js");
 /* harmony import */ var _components_accordion__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/accordion */ "./src/js/lib/components/accordion.js");
 /* harmony import */ var _components_carousel__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./components/carousel */ "./src/js/lib/components/carousel.js");
+/* harmony import */ var _services_requests__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./services/requests */ "./src/js/lib/services/requests.js");
+
 
 
 
@@ -795,6 +797,48 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.click = function (handle
 
 /***/ }),
 
+/***/ "./src/js/lib/services/requests.js":
+/*!*****************************************!*\
+  !*** ./src/js/lib/services/requests.js ***!
+  \*****************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.get = async function (url, dataTypeAnswer = 'json') {
+  let res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  }
+  switch (dataTypeAnswer) {
+    case 'json':
+      return await res.json();
+    case 'text':
+      return await res.text();
+    case 'blob':
+      return await res.blob();
+  }
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.post = async function (url, data, dataTypeAnswer = 'text') {
+  let res = await fetch(url, {
+    method: 'POST',
+    body: data
+  });
+  switch (dataTypeAnswer) {
+    case 'json':
+      return await res.json();
+    case 'text':
+      return await res.text();
+    case 'blob':
+      return await res.blob();
+  }
+};
+
+/***/ }),
+
 /***/ "./src/js/main.js":
 /*!************************!*\
   !*** ./src/js/main.js ***!
@@ -820,14 +864,13 @@ $('#trigger').click(() => $('#trigger').createModal({
     }]]
   }
 }));
-window.addEventListener('DOMContentLoaded', () => {
-  $('#example2').createCarousel({
-    dots: true,
-    arrows: false,
-    count: 4,
-    content: ["https://www.flixwatch.co/wp-content/uploads/80238012.jpg.webp", "https://www.pinkvilla.com/images/2023-06/1611187259_black-clover-sword-of-the-wizard-king.jpg", "https://m.economictimes.com/thumb/msid-100947486,width-1200,height-628,resizemode-4,imgsize-110074/black-clover-chapter-362-see-release-date-and-time.jpg", "https://www.pinkvilla.com/images/2023-06/1611187259_black-clover-sword-of-the-wizard-king.jpg"]
-  });
+$('#example2').createCarousel({
+  dots: true,
+  arrows: false,
+  count: 4,
+  content: ["https://www.flixwatch.co/wp-content/uploads/80238012.jpg.webp", "https://www.pinkvilla.com/images/2023-06/1611187259_black-clover-sword-of-the-wizard-king.jpg", "https://m.economictimes.com/thumb/msid-100947486,width-1200,height-628,resizemode-4,imgsize-110074/black-clover-chapter-362-see-release-date-and-time.jpg", "https://www.pinkvilla.com/images/2023-06/1611187259_black-clover-sword-of-the-wizard-king.jpg"]
 });
+$().get('https://jsonplaceholder.typicode.com/todos/1').then(res => console.log(res));
 
 /***/ })
 
