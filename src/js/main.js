4402a430d5146a49ffcef6627858242ec1d9abca
KoFE <petrovykh03@gmail.com>
2023-07-23 13:16:38 +0500

    Dynamic model creation added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,34 @@
 import './lib/lib';
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae sint, praesentium excepturi obcaecati magnam quasi consequuntur tenetur architecto doloremque omnis minima eaque ipsa quae repellat doloribus exercitationem quis dolor?'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Data saved');
+                }
+            ],
+            [
+                'Another button',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('hello');
+                }
+            ]
+        ]
+    }
+}));
